045fdd73e50d31be9c87abbce9adbf105a2f8d0b
Stephen Hicks <sdh33@cornell.edu>
2013-07-16 01:21:11 -0700

    Munging with an object

---
+++ Foo.java
@@ -0,0 +1,47 @@
+// Copyright statement
+
+package com.google.common.base;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * Class-level javadoc
+ * @author foo
+ */
+@SomeAnnotation("bar")
+public class Foo<T extends Foo<T>> extends Bar<List<T>> implements Baz, Qux<T> {
+
+  @FieldType(type = Thingy.FOO)
+  private static final int MY_NUM;
+
+  @FlagSpec(name = "foo", count = 23)
+  private static final Flag<String> myFlag = Flag.value("blah");
+
+  static {
+    MY_NUM = 54;
+  }
+
+  public enum Thingy implements Bar<Integer> {
+    ABC,
+    DEF(23),
+    GHI {
+      @Override public void run() {}
+    };
+    public void run() {
+        execute();  // TODO(sdh): FOR SOME REASON, passing an argument makes us think execute is a primary rather than a method...?
+    }
+    JKL() {
+      public final Thingy next = ABC;
+    },;
+    Thingy() {}
+    Thingy(int num) {}
+  }
+
+  /** Some more javadoc */  
+}
+;
